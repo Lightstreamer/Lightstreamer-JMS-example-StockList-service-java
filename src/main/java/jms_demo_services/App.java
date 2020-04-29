6524c576d0b13f6a4382d4e9b32e6676b3fca9e9
Gianluca Finocchiaro <gianluca.finocchiaro@gmail.com>
2020-04-29 10:45:01 +0200

    Update looking up of properties

--- src/main/java/jms_demo_services/App.java
+++ src/main/java/jms_demo_services/App.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  *
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
@@ -28,6 +28,10 @@ public class App {
 
   private static Logger log = LoggerFactory.getLogger(App.class);
 
+  private static final String DEFAULT_BROKER = "ActiveMQ";
+
+  private static final String broker = System.getProperty("broker", DEFAULT_BROKER);
+
   public static void main(String[] args) {
     try (InputStream is = App.class.getResourceAsStream("/service.conf")) {
       log.info("StockList Demo service starting. Loading configuration...");
@@ -38,11 +42,11 @@ public class App {
       // Read parameters
       //@formatter:off
       Configuration config =  new Configuration.Builder()
-        .withJmsURL(props.getProperty("jmsUrl"))
-        .withInitialiContextFactory(props.getProperty("initialContextFactory"))
+        .withJmsURL(getProperty(props, "jmsUrl"))
+        .withInitialiContextFactory(getProperty(props, "initialContextFactory"))
         .withConnectionFactoryName(props.getProperty("connectionFactoryName"))
-        .withTopic(props.getProperty("topic"))
-        .withCredentials(props.getProperty("user"), props.getProperty("password"))
+        .withTopicName(props.getProperty("topicName"))
+        .withCredentials(getProperty(props, "user"), getProperty(props, "password"))
         .build();
       //@formatter:on
 
@@ -55,5 +59,10 @@ public class App {
         log.error("Exception caught while starting StockList Demo service: " + e.getMessage(), e);
       }
     }
+  }
+
+  private static String getProperty(Properties props, String key) {
+    return props.getProperty(broker + "." + key);
+
   }
 }
